18fa99979738b8816a2d4256803ccdb1689ffef0
sonace11 <sonny-nguyen@live.nmit.ac.nz>
2024-03-07 14:59:29 +1300

    more js lession

--- number.js
+++ number.js
@@ -4,11 +4,42 @@ let: you can change the varibel later
 const: you cant change the varibel 
 var: old version of let and const
 */
-const num1 = 10
-console.log(num1)
+// const pi = 3.14
+// //pi = 10
+// console.log(pi)
 
-let num2 = 20
-console.log(num2)
-var num3 = 30
-console.log(num3)
+// let num2 =20
+// num2= 25
+// console.log(num2)
+// var num3 = 30
+// console.log(num3)
+
+// const fullname = 'son nguyen'
+// console.log(fullname)
+
+// const workplace='NMIT'
+// console.log(workplace)
+
+// console.log(fullname + " "+workplace)
+
+// console.log(`my fullname is ${fullname} and i work at ${workplace} `)
+
+// const working = true
+// const notworking = false 
+
+// if(pi == 3.14) {
+//     console.log(fullname)}
+
+    let a =2 
+    let result
+// if (){} else {}
+a = 0
+    if (a >= 0) {
+        result = 'positive';
+    console.log(result)
+}else {
+    result = 'not positive';
+    console.log(result)
+}
+
 //
