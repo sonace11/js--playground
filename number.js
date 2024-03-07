ec161d790410cb54695c5058f42b72849c3a0489
sonace11 <sonny-nguyen@live.nmit.ac.nz>
2024-03-07 13:50:56 +1300

    lern let const var

---
+++ number.js
@@ -0,0 +1,14 @@
+//NOTE - 
+/*
+let: you can change the varibel later 
+const: you cant change the varibel 
+var: old version of let and const
+*/
+const num1 = 10
+console.log(num1)
+
+let num2 = 20
+console.log(num2)
+var num3 = 30
+console.log(num3)
+//
