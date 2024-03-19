f0a79bc065be803242fd9db3198d322a8478cbcb
sonace11 <sonny-nguyen@live.nmit.ac.nz>
2024-03-19 15:40:42 +1300

    more array skill

--- functions-2.js
+++ functions-2.js
@@ -29,32 +29,47 @@ output -> 1+6+11 = 18
 
 */
 
-function sumMinArrays(arr){
-    let sumTotal = 0;
+// function sumMinArrays(arr){
+//     let sumTotal = 0;
 
-    for(let i =0; i < arr.length; i++ ) {
-    let y = arr[i][0]
+//     for(let i =0; i < arr.length; i++ ) {
+//     let y = arr[i][0]
     
-    for ( let x =0; x<arr[i].length; x++ ) {
-    if (y > arr[i][x]) {
-        y = arr[i][x];
-    }
-}
-sumTotal += y
-}
-return sumTotal
-}
-console.log(sumMinArrays([
-    [1,2,3,4,5],
-    [6,7,8,9,10],
-    [11,12,13,14,15]
- ])) 
+//     for ( let x =0; x<arr[i].length; x++ ) {
+//     if (y > arr[i][x]) {
+//         y = arr[i][x];
+//     }
+// }
+// sumTotal += y
+// }
+// return sumTotal
+// }
+// console.log(sumMinArrays([
+//     [1,2,3,4,5],
+//     [6,7,8,9,10],
+//     [11,12,13,14,15]
+//  ])) 
 
 
-//ES6 
-const sumMinArraysNew = arr => arr.reduce((a,b) => a + Math.min(...b),0)
-console.log(sumMinArrays([
-    [1,2,3,4,5],
-    [6,7,8,9,10],
-    [11,12,13,14,15]
- ])) 
+// //ES6 
+// const sumMinArraysNew = arr => arr.reduce((a,b) => a + Math.min(...b),0)
+// console.log(sumMinArrays([
+//     [1,2,3,4,5],
+//     [6,7,8,9,10],
+//     [11,12,13,14,15]
+//  ])) 
+
+ // create a function that takes an array, return duplicate numbers in new array.
+ // ArraysDuplicateNumbers([1,2,3,4,5,6,5,1]) output -> [5,1]
+
+ 
+ //ES6 a function that show what numbers is dup in the array 
+
+ const ArraysDuplicateNumbers = arr => (
+    arr= [...new Set([...arr.filter(num =>
+        arr.indexOf(num) !== arr.lastIndexOf(num)
+    )])].sort((a,b) => a-b),
+    arr.length ? arr: null
+ )
+ console.log(ArraysDuplicateNumbers([5,1,2,3,4,5,6,1]))
+
